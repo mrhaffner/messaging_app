63c70f628caac7f3194b572865c1268009d9f1fc
mrhaffner <mrhaffner13@gmail.com>
2023-02-10 20:21:37 -0600

    updated main_window with empty components

---
+++ client/component/main_window.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+# displays the other items in this file
+class ChatView(tk.Frame):
+    pass
+
+
+# contains a dropdown of users to message (default is group)
+# contains an input box for your message to send
+# contains a button to send the message
+class MessageBar(tk.Frame):
+    pass
+
+
+# displays a list of all users currently online that aren't you
+class UsersList(tk.Frame):
+    pass
+
+
+# displays the title, your username, and a logout button
+class TitleBar(tk.Frame):
+    pass
+
+
+# displays all the messages in chat
+class MessageList(tk.Frame):
+    pass
