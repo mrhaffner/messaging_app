5f3d7ff7680d36bdf06ddc0ec51dc6752765be6b
mrhaffner <mrhaffner13@gmail.com>
2023-02-15 18:42:30 -0600

    added server models

--- server/model/message.py
+++ server/model/message.py
@@ -1,6 +1,33 @@
-class MessageDTO:
-    pass
+import json
+from dataclasses import asdict, dataclass
+
+from shared import SingletonMeta
+from user import User
 
 
+@dataclass(frozen=True)
 class Message:
-    pass
+    id: int = field(init=False, default=Message.get_unique_id())
+    text: str = field(compare=False)
+    sender: User = field(compare=False)
+    receiver: User = field(compare=False)
+
+    next_id = 1
+
+    def to_dto(self):
+        return json.dumps(asdict(self)) # works with User?
+
+    @staticmethod
+    def get_unique_id():
+        new_id = Message.next_id
+        Message.next_id += 1
+        return new_id
+
+    @staticmethod
+    def from_dto(dto):
+        message_dict = json.loads(dto)
+        return Message(
+                        message_dict['text'], 
+                        User.from_dto(message_dict['sender']),  # necessary?
+                        User.from_dto(message_dict['receiver'])  # necessary?
+                      )
--- server/model/user.py
+++ server/model/user.py
@@ -1,10 +1,44 @@
-class UserDTO:
-    pass
+import json
+from dataclasses import asdict, dataclass
+
+from shared import SingletonMeta
 
 
+@dataclass(frozen=True)
 class User:
-    pass
+    name: str
+
+    def to_dto(self):
+        return json.dumps(asdict(self))
+
+    @staticmethod
+    def from_dto(dto):
+        user_dict = json.loads(dto)
+        return User(user_dict['name'])
 
 
-class UserList:
-    pass
+# code reuse ?
+class UserList(metaclass=SingletonMeta):
+
+    users = set()
+
+    def add(self, user):
+        if self.exists(user):
+            raise ValueError("User already exists")
+        self.users.add(user)
+
+    def add_from_dto(self, user_dto):
+        self.add(User.from_dto(user_dto))
+
+    # def add_many_from_dtos(self, user_dtos):
+    #     for user_dto in user_dtos:
+    #         self.add_from_dto(user_dto)
+
+    def remove(self, user):
+        self.users.discard(user)
+
+    def exists(self, user):
+        return user in self.users
+
+    def get_all(self):
+        return list(self.users)
