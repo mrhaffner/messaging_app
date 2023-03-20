f90a001fd54289f07d5ee4cfd38662bf74431216
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-03-20 13:57:40 -0500

    added event handler for button click to login user

--- client/component/login_popup.py
+++ client/component/login_popup.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from controller import *
+from component.main_window import ChatPage
 
 # displays the login form
 class LoginPopup(ttk.Frame):
@@ -27,7 +29,7 @@ class LoginPopup(ttk.Frame):
         self.passwordEntry.insert(0, self.PASSWORD_TEXT)
 
         # MISSING command to call handler to log in
-        self.logInBtn = tk.Button(self, text="Log In")
+        self.logInBtn = tk.Button(self, text="Log In", command=self.on_button_click)
 
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~   GRID CONFIGURATIONS   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
@@ -41,19 +43,30 @@ class LoginPopup(ttk.Frame):
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~   BINDINGS   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         #self.entry.bind("<Return>", self.send_message) # binds hitting the enter key to the send_message() event handler
-        self.usernameEntry.bind("<FocusIn>", self.on_username_entry_click) # gets rid of the "Enter text here..." when clicking into the Entry
-        self.usernameEntry.bind("<FocusOut>", self.on_focusout) # brings back the "Enter text here..." when clicking off of the Entry
-        self.passwordEntry.bind("<FocusIn>", self.on_pw_entry_click) # gets rid of the "Enter text here..." when clicking into the Entry
-        self.passwordEntry.bind("<FocusOut>", self.on_focusout) # brings back the "Enter text here..." when clicking off of the Entry
+        self.usernameEntry.bind("<FocusIn>", self.on_username_entry_click)
+        self.passwordEntry.bind("<FocusIn>", self.on_pw_entry_click) 
+        self.usernameEntry.bind("<FocusOut>", self.on_focusout) 
+        self.passwordEntry.bind("<FocusOut>", self.on_focusout) 
+    
+    def on_button_click(self):
+        if (self.usernameEntry.get() != self.USERNAME_TEXT and self.passwordEntry.get() != self.PASSWORD_TEXT):
+            print('first check')
+            if (login(self.usernameEntry.get(), self.passwordEntry.get())):
+                print('user logged in')
+                # switch page to ChatPage - not sure if there is a better way to do this or if this is even correct
+                chat_page = ChatPage
+                chat_page.tkraise() 
 
+    # gets rid of the "Enter text here..." when clicking into the Entry
     def on_username_entry_click(self, event):
         if self.usernameEntry.get() == self.USERNAME_TEXT:
             self.usernameEntry.delete(0, "end")
 
+    # gets rid of the "Enter text here..." when clicking into the Entry
     def on_pw_entry_click(self, event):
         if self.passwordEntry.get() == self.PASSWORD_TEXT:
             self.passwordEntry.delete(0, "end")
-    
+    # brings back the "Enter text here..." when clicking off of the Entry
     def on_focusout(self, event):
         if self.usernameEntry.get() == "":
             self.usernameEntry.insert(0, self.USERNAME_TEXT)
