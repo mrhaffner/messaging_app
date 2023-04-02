d5a7c18e5c26c06a99b1835c0eeca3a3e7a7dcc8
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-04-02 13:40:23 -0500

    fixed issue with logout button and added current user label

--- client/component/main_window.py
+++ client/component/main_window.py
@@ -42,34 +42,49 @@ class ChatPage(ttk.Frame):
         self.send_button = ttk.Button(self, text="Send", command=self._send_message)
 
         # Create scrolledtext widget
-        self.scrolled_text_chatbox = scrolledtext.ScrolledText(self, wrap='word')
+        self.chatbox_scrolled_text = scrolledtext.ScrolledText(self, wrap='word')
 
         # Create input area
         self.entry = ttk.Entry(self, style='Custom.TEntry')
         self.entry.insert(0, self.ENTER_TEXT_HERE)
 
         # Create logout button 
-        self.logout_button = ttk.Button(self, text="Log Out", command=self._logout) 
+        self.logout_button = ttk.Button(self, text="Log out", command=self._logout) 
+
+        # TODO: Pass a method to 'text' parameter to dynamically show client side username
+        self.current_user_label = ttk.Label(self, text="Current User", font=('Arial', '10', 'bold'))
 
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~   GRID CONFIGURATIONS   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
-        self.online_users_label.grid(row=0, column=2, sticky="w", padx=10, pady=2)
 
-        self.online_users_listbox.grid(row=1, column=2, sticky="nsew", padx=10)
-
+        # ROW 0
         self.chat_room_label.grid(row=0, column=0, sticky="w", padx=10, pady=2)
+        self.current_user_label.grid(row=0, column=1, sticky="e", padx=10, pady=2)
+        self.logout_button.grid(row=0, column=2, sticky="e", padx=10, pady=2)
+        self.online_users_label.grid(row=0, column=3, sticky="w", padx=10, pady=2)
 
-        self.scrolled_text_chatbox.grid(row=1, column=0, rowspan=1, columnspan=2, sticky="nsew", padx=10)
-        self.scrolled_text_chatbox.config(state='disabled')
+        # ROW 1
+        self.chatbox_scrolled_text.grid(row=1, column=0, rowspan=1, columnspan=3, sticky="nsew", padx=10)
+        self.online_users_listbox.grid(row=1, column=3, sticky="nsew", padx=10)
 
-        self.user_dropdown_combobox.grid(row=2, column=2, sticky="sew", padx=10, pady=5)
+        # ROW 2
+        self.entry.grid(row=2, column=0, columnspan=3, sticky="nsew", padx=10, pady=10)
+        self.send_button.grid(row=2, column=3, sticky="nwe", padx=10, pady=5)
+        self.user_dropdown_combobox.grid(row=2, column=3, sticky="sew", padx=10, pady=5)
 
-        self.entry.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
 
-        self.send_button.grid(row=2, column=2, sticky="nwe", padx=10, pady=5)
-
-        self.logout_button.place(relx=0.77, rely=0, anchor="ne")
+        # --------------------- {{  ORIGINAL GRID CONFIG  }} -----------------------------
+        # self.online_users_label.grid(row=0, column=2, sticky="w", padx=10, pady=2)\
+        # self.online_users_listbox.grid(row=1, column=2, sticky="nsew", padx=10)
+        # self.chat_room_label.grid(row=0, column=0, sticky="w", padx=10, pady=2)
+        # self.current_user_label.grid(row=0, column=0, sticky="w", padx=10, pady=2)
+        # self.scrolled_text_chatbox.grid(row=1, column=0, rowspan=1, columnspan=2, sticky="nsew", padx=10)
+        # self.scrolled_text_chatbox.config(state='disabled')
+        # self.user_dropdown_combobox.grid(row=2, column=2, sticky="sew", padx=10, pady=5)
+        # self.entry.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+        # self.send_button.grid(row=2, column=2, sticky="nwe", padx=10, pady=5)
+        # self.logout_button.place(relx=0.77, rely=0, anchor="ne")
 
         # Configure grid weights
         self.columnconfigure(1, weight=1)
@@ -114,11 +129,11 @@ class ChatPage(ttk.Frame):
         if message:
             self.parent.send_message(message, user_name)
             # Enable the chatbox to insert the message
-            self.scrolled_text_chatbox.config(state='normal')
+            self.chatbox_scrolled_text.config(state='normal')
             # Add message to chatbox
-            self.scrolled_text_chatbox.insert(tk.END, f"{message}\n")
+            self.chatbox_scrolled_text.insert(tk.END, f"{message}\n")
             # Disable the chat box
-            self.scrolled_text_chatbox.config(state='disable')
+            self.chatbox_scrolled_text.config(state='disable')
             self.entry.delete('0', 'end')
         return "break" # prevents the default behavior of the "Return"
     
@@ -144,8 +159,8 @@ class ChatPage(ttk.Frame):
     
     # Updates the list of message entries in the chat room. 
     def _update_message_list_entries(self, messages):
-        self.scrolled_text_chatbox.config(state='normal')
-        self.scrolled_text_chatbox.delete('1.0', tk.END)
+        self.chatbox_scrolled_text.config(state='normal')
+        self.chatbox_scrolled_text.delete('1.0', tk.END)
         for message in messages:
-            self.scrolled_text_chatbox.insert(tk.END, message.text + '\n')
-        self.scrolled_text_chatbox.config(state='disabled')
+            self.chatbox_scrolled_text.insert(tk.END, message.text + '\n')
+        self.chatbox_scrolled_text.config(state='disabled')
