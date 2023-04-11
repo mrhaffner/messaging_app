e3bd3a813a2f6e531c7f9f565ac86da53b9f3fa4
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-04-11 16:20:21 -0500

    added methods to kick user and show the kick btn

--- client/component/main_window.py
+++ client/component/main_window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, scrolledtext
+import typing
 
 
 
@@ -11,6 +12,9 @@ class ChatPage(ttk.Frame):
 
         # Constants
         self.ENTER_TEXT_HERE = "Enter text here..."
+
+        # set focus on the this window, which means it will capture events such as key presses including escape key events
+        self.focus_set()
 
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~   STYLING   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
@@ -25,7 +29,7 @@ class ChatPage(ttk.Frame):
         self.online_users_label = ttk.Label(self, text="Online users", font=('Arial', '10', 'bold'))
 
         # Create listbox for online users using dummy data for now
-        self.online_users_listbox = tk.Listbox(self, height=20)
+        self.online_users_listbox = tk.Listbox(self, selectmode="single", height=20)
 
         # Create dropdown menu for selecting a user
         self.user_var = tk.StringVar(self)
@@ -33,7 +37,6 @@ class ChatPage(ttk.Frame):
         # I think we're gonna want to set the state to readonly right off the bat
         # I'm not sure if we'll be able to select a user if its readonly just yet, so this may need to be changed to 'normal' here.
         self.user_dropdown_combobox['state'] = 'readonly' 
-        # self.user_dropdown.current(0)
 
         # Create Chat Room label
         self.chat_room_label = ttk.Label(self, text="Chat Room", font=('Arial', '10', 'bold'))
@@ -52,8 +55,11 @@ class ChatPage(ttk.Frame):
         # Create logout button 
         self.logout_button = ttk.Button(self, text="Log out", command=self._logout) 
 
-        # TODO: Pass a method to 'text' parameter to dynamically show client side username
+        # Creates the current user label, i.e., @admin
         self.current_user_label = ttk.Label(self, text="@", font=('Arial', '10', 'bold'))
+
+        # Creates the kick button that the admin uses to kick users
+        self.kick_button = ttk.Button(self, text="Kick", command=self._select_user_to_kick) 
 
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
         ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~   GRID CONFIGURATIONS   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
@@ -64,27 +70,16 @@ class ChatPage(ttk.Frame):
         self.current_user_label.grid(row=0, column=1, sticky="e", padx=10, pady=2)
         self.logout_button.grid(row=0, column=2, sticky="e", padx=10, pady=2)
         self.online_users_label.grid(row=0, column=3, sticky="w", padx=10, pady=2)
+        # Kick Button will be added to the grid based on the condition that the current user is an admin (show_kick_button method)
 
         # ROW 1
         self.chatbox_scrolled_text.grid(row=1, column=0, rowspan=1, columnspan=3, sticky="nsew", padx=10)
-        self.online_users_listbox.grid(row=1, column=3, sticky="nsew", padx=10)
+        self.online_users_listbox.grid(row=1, column=3, columnspan=2, sticky="nsew", padx=10)
 
         # ROW 2
         self.entry.grid(row=2, column=0, columnspan=3, sticky="nsew", padx=10, pady=10)
-        self.send_button.grid(row=2, column=3, sticky="nwe", padx=10, pady=5)
-        self.user_dropdown_combobox.grid(row=2, column=3, sticky="sew", padx=10, pady=5)
-
-        # --------------------- {{  ORIGINAL GRID CONFIG  }} -----------------------------
-        # self.online_users_label.grid(row=0, column=2, sticky="w", padx=10, pady=2)\
-        # self.online_users_listbox.grid(row=1, column=2, sticky="nsew", padx=10)
-        # self.chat_room_label.grid(row=0, column=0, sticky="w", padx=10, pady=2)
-        # self.current_user_label.grid(row=0, column=0, sticky="w", padx=10, pady=2)
-        # self.scrolled_text_chatbox.grid(row=1, column=0, rowspan=1, columnspan=2, sticky="nsew", padx=10)
-        # self.scrolled_text_chatbox.config(state='disabled')
-        # self.user_dropdown_combobox.grid(row=2, column=2, sticky="sew", padx=10, pady=5)
-        # self.entry.grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
-        # self.send_button.grid(row=2, column=2, sticky="nwe", padx=10, pady=5)
-        # self.logout_button.place(relx=0.77, rely=0, anchor="ne")
+        self.send_button.grid(row=2, column=3, columnspan=2, sticky="nwe", padx=10, pady=5)
+        self.user_dropdown_combobox.grid(row=2, column=3, columnspan=2, sticky="sew", padx=10, pady=5)
 
         # Configure grid weights
         self.columnconfigure(1, weight=1)
@@ -100,6 +95,7 @@ class ChatPage(ttk.Frame):
         self.entry.bind("<FocusIn>", self._on_entry_click) # gets rid of the "Enter text here..." when clicking into the Entry
         self.entry.bind("<FocusOut>", self._on_focusout) # brings back the "Enter text here..." when clicking off of the Entry
         # self.user_dropdown_combobox.bind('<<ComboboxSelected>>', self._select_user) # calls a method that print the user that is selected (no real functionality atm)
+        # TODO: Should we log the user out if they hit escape within the ChatPage? 
 
     # Event handler for <FocusIn> event
     def _on_entry_click(self, event):
@@ -126,20 +122,42 @@ class ChatPage(ttk.Frame):
 
         if message:
             self.parent.send_message(message, user_name)
-            # Enable the chatbox to insert the message
-            # self.scrolled_text_chatbox.config(state='normal')
-            # # Add message to chatbox
-            # self.scrolled_text_chatbox.insert(tk.END, f"{message}\n")
-            # # Disable the chat box
-            # self.scrolled_text_chatbox.config(state='disable')
-            # self.entry.delete('0', 'end')
 
         return "break" # prevents the default behavior of the "Return"
     
-    # TODO: Error handling
     # TODO: Provide other means for the user to log out besides hitting the log out button
     def _logout(self):
         self.parent.log_out()
+
+    def _kick_user(self, user_name, index):
+    # def _kick_user(self, user_name):
+        self.parent.kick_user(user_name) # Calls kick_user method on View
+        self.online_users_listbox.delete(index)
+    
+    def _select_user_to_kick(self):
+        selected_users = self.online_users_listbox.curselection() # returns tuple with length of 1 or 0
+
+        print(f'selected_indices: {selected_users}. length={len(selected_users)}')
+
+        if len(selected_users) == 1: # Makes sure something is selected
+            index_of_user_to_kick = selected_users[0]
+            selected_user: str = self.online_users_listbox.get(index_of_user_to_kick)
+
+            print(f'type: {type(selected_user)} selected_username: {selected_user}')
+
+            self._kick_user(selected_user, index_of_user_to_kick)
+            self._remove_user_from_combobox(selected_user)
+    
+    # Private method that deletes removes the user from the comboxbox after the admin kicks a user
+    def _remove_user_from_combobox(self, user_name):
+        if user_name != "group":
+            index = self.user_dropdown_combobox["values"].index(user_name)
+            self.user_dropdown_combobox.delete(index)
+
+    # Shows the kick button if the CurrentUser is the Admin
+    def show_kick_button(self):
+        self.kick_button.grid(row=0, column=4, sticky="e", padx=10, pady=2)
+        self.online_users_listbox.config(state='normal') # Enables the list box for the admin
     
     # Updates the list box that contains the online users, not the drop down menu.
     def _update_user_listbox(self, users):
@@ -148,6 +166,13 @@ class ChatPage(ttk.Frame):
         for user in users:
             self.online_users_listbox.insert(tk.END, user.name)
         self.online_users_listbox.config(state='disabled')
+
+    # Updates the list box that contains the online users, not the drop down menu.
+    def _update_user_listbox_unrestricted(self, users):
+        self.online_users_listbox.config(state='normal')
+        self.online_users_listbox.delete(0, tk.END)
+        for user in users:
+            self.online_users_listbox.insert(tk.END, user.name)
     
     # Updates the drop down menu of users that the user can select to send messages to. 
     def _update_user_dropdown_combobox(self, users):
